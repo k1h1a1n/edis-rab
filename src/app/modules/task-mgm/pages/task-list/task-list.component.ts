5073c55d99f8c46a748fb18e0af79ea3d2f75cae
k1h1a1n <affykhan789678@gmail.com>
2022-03-01 03:16:49 +0530

    listingfrom_nosaj

--- src/app/modules/task-mgm/pages/task-list/task-list.component.ts
+++ src/app/modules/task-mgm/pages/task-list/task-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TaskdetailsService } from '../../services/taskdetails.service';
 
 @Component({
   selector: 'app-task-list',
@@ -6,10 +7,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./task-list.component.scss']
 })
 export class TaskListComponent implements OnInit {
+  taskDetails = [];
+  selectedTab : any;
+  selectedTabtaskDetails = [];
+  taskDetailsHdrTabs =[];
+  constructor(public taskdetailservice: TaskdetailsService) { 
 
-  constructor() { }
+    this.taskDetails = taskdetailservice.gettaskDetailsList();
+    if( this.taskDetails?.length == 0 ){
+      taskdetailservice.loadTaskDetails().subscribe( taskDetails => {
+        this.taskDetails = taskDetails;
+        this.processtaskDetails();
+      });
+    }
+    else{
+      this.processtaskDetails();
+    }
+  }
 
   ngOnInit(): void {
   }
+
+  processtaskDetails()
+  {
+    if( this.taskDetails?.length > 0 )
+    {
+      this.taskDetailsHdrTabs = this.taskDetails.filter( taskDetails => {
+        return taskDetails.type == 'header';
+      });
+      this.selecttaskdetailTab(this.taskDetailsHdrTabs[0]);
+    }
+  }
+
+  selecttaskdetailTab(taskdetailTab){
+    this.selectedTab = taskdetailTab;
+    this.selectedTabtaskDetails = this.taskDetails.filter( taskdetail => {
+      return taskdetail.header == this.selectedTab.id;
+    });
+  }
+
 
 }
